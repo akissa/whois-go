2914f5be88e634debf764f1deb414393d0dba648
Andrew Colin Kissa <andrew@topdog.za.net>
2018-05-03 14:19:07 +0200

    Use the new API for the cmd

--- whois/main.go
+++ whois/main.go
@@ -9,31 +9,30 @@
 
 package main
 
-
 import (
-    "fmt"
-    "os"
-    "github.com/likexian/whois-go"
+	"fmt"
+	"os"
+
+	"github.com/akissa/whois-go"
 )
 
-
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println(fmt.Sprintf("usage:\n\t%s domain [server]", os.Args[0]))
-        os.Exit(1)
-    }
+	if len(os.Args) < 3 {
+		fmt.Println(fmt.Sprintf("usage:\n\t%s domain chaseReferal [server]", os.Args[0]))
+		os.Exit(1)
+	}
 
-    var server string
-    if len(os.Args) > 2 {
-        server = os.Args[2]
-    }
+	var server string
+	if len(os.Args) > 2 {
+		server = os.Args[2]
+	}
 
-    result, err := whois.Whois(os.Args[1], server)
-    if err != nil {
-        fmt.Println(err.Error())
-        os.Exit(1)
-    }
+	result, err := whois.Whois(os.Args[1], true, server)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 
-    fmt.Println(result)
-    os.Exit(0)
+	fmt.Println(result)
+	os.Exit(0)
 }
